49dea359041e4bcfe01b2479fd20ab3c41a2ce95
CodeByBlazej <codebyblazej@gmail.com>
2023-11-19 13:27:55 +0000

    Update app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// let name = 'Blazej';
+
+// if (name = 'Blazej') {
+//   hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+// }
+
+// function greet() {
+//   let age = 30;
+//   let name = 'Max';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet(); 
+// 'use strict';
+
+const userName = 'Blazej';
+let undefined = 5;
+
+
+console.log(userName);
